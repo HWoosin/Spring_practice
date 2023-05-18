dfefa340eaab8aa457ac594f9cc979121263ca5b
woosin <ws1340cc@naver.com>
2023-05-18 10:36:52 +0900

    userMapper를 test완료 230518

--- MyWeb/src/test/java/com/spring/myweb/user/mapper/UserMapperTest.java
+++ MyWeb/src/test/java/com/spring/myweb/user/mapper/UserMapperTest.java
@@ -1,7 +1,11 @@
 package com.spring.myweb.user.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,7 +47,14 @@ public class UserMapperTest {
 	@DisplayName("존재하는 회원 아이디와 올바른 비밀번호를 입력했을 시"
 			+ "회원의 정보가 리턴되어야 한다.")
 	void loginTest() {
-		
+		String id = "abc1234";
+		String pw = "aaa1111";
+		/*
+		Map<String, String> data = new HashMap<>();
+		data.put("userId", "abc1234");
+		data.put("userPw", "aaa1111");
+		*/
+		assertNotNull(mapper.login(id, pw));
 		
 	}
 	
@@ -59,16 +70,16 @@ public class UserMapperTest {
 	@Test
 	@DisplayName("id를 제외한 회원의 정보를 수정할 수 있다.")
 	void updateTest() {
-		String id = "abc1234";
 		UserVO vo = new UserVO();
 		vo.setUserId("abc1234");
-		
-		if(mapper.idCheck(id)==1) {
-			vo.setUserPw("aaa1111");
-			vo.setUserName("홍길동이");
-		}
+		vo.setUserPw("aaa1111");
+		vo.setUserName("홍수정");
+		vo.setUserEmail1("abc1234");
+		vo.setUserEmail2("naver.com");
 
 		mapper.updateUser(vo);
+		
+		assertEquals(mapper.getInfo("abc1234").getUserName(), vo.getUserName());
 	}
 	
 	
